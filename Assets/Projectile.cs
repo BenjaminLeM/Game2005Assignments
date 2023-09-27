bad1c996284d614b1aa6749f87c72527a0865471
BenjaminLeM <Benjamin.LeMaistre@georgebrown.ca>
2023-09-26 21:56:31 -0400

    added x and y being affected by the launch angle
    note: ball launches into orbit for no reason

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public float launchVelocity;
-    public float launchAngle;
-    public float launchHeight;
+    public float launchVelocity = 10.0f;
+    public float launchAngle = 30.0f;
+    public float launchHeight = 3.0f;
 
     private float velX = 10.0f;  // Doesn't change between frames
     private float velY = 10.0f;  // Under constant acceleration (gravity)
@@ -18,10 +18,11 @@ public class Projectile : MonoBehaviour
 
     void LaunchBall()
     {
-        
-        transform.position = new Vector3(transform.position.x, launchHeight, transform.position.z);
         // 1. Update velX and velY based on horizontal and vertical components of launch velocity & launch angle
+        velX =  Mathf.Cos(launchAngle) * launchVelocity;
+        velY =  Mathf.Sin(launchAngle) * launchVelocity;
         // 2. Assign position to new launch height and re-launch the ball!
+        transform.position = new Vector3(-12.0f, launchHeight, 32.0f);
     }
 
     void FixedUpdate()
