ab1994b9a618b3dec0ccd9d8dd20cbe354aa3d18
BenjaminLeM <Benjamin.LeMaistre@georgebrown.ca>
2023-10-04 22:57:23 -0400

    Changed ProjectileBody
    projectile now moves

--- Assets/Scripts/ProjectileBody.cs
+++ Assets/Scripts/ProjectileBody.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ProjectileBody : Body
 {
     // Homework: Incorporate launch position, launch speed, and launch angle into our new custom-physics system!
     // Plot 3D projectile motion by specifying a pitch (launch-angle about X) and yaw (launch-angle about Y)
     public Vector3 launchPosition;
-    public float launchSpeed;
-    public float launchPitch = 0.0f;
-    public float launchYaw = 0.0f;
+    public float launchSpeed = 5.0f;
+    public float launchPitch = 50.0f;
+    public float launchYaw = 25.0f;
+    public float velX;
+    public float velY;
+    public float velZ;
+    public float airResistance = 0.91f;
+    public float terminalVelocity = 500.0f;
+    float dt = Time.fixedDeltaTime;
 
     void Launch()
     {
         // TODO -- Decompose pitch and yaw into velocity x, y & z!
+        velX = (Mathf.Cos(Mathf.Deg2Rad * launchYaw)*Mathf.Cos(Mathf.Deg2Rad * launchPitch));
+        velY = (Mathf.Sin(Mathf.Deg2Rad * launchYaw)*Mathf.Cos(Mathf.Deg2Rad * launchPitch));
+        velZ = (Mathf.Sin(Mathf.Deg2Rad * launchPitch));
         // TODO -- Add controls to change the direction of the acceleration (key input or GUI slider)
 
         // TODO -- Add drag (air resistance) to the simulation (similar to exercise 2 where we dampened the velocity on-bounce)
@@ -36,5 +46,10 @@ public class ProjectileBody : Body
             Launch();
         }
         Simulate(Physics.gravity, Time.fixedDeltaTime);
+        transform.position = new Vector3(
+            transform.position.x + velX,
+            transform.position.y + velY,
+            transform.position.z + velX
+        );
     }
 }
