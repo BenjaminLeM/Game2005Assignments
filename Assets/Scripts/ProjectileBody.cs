3a10ca8b06c81373c0b5a8bca31afe65ada8e8a8
BenjaminLeM <Benjamin.LeMaistre@georgebrown.ca>
2023-10-20 22:58:28 -0400

    Added function plane sphere collision

--- Assets/Scripts/ProjectileBody.cs
+++ Assets/Scripts/ProjectileBody.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 
@@ -28,6 +29,24 @@ public class ProjectileBody : MonoBehaviour
         float distance = displacement.magnitude;
         return distance < bodyA.radius + bodyB.radius;
     }
+    bool checkSpherePlaneCollision(Body bodyA, Body bodyB)
+    {
+        float distance = Mathf.Abs(bodyB.transform.position.x * bodyA.transform.position.x 
+                                    + bodyB.transform.position.y * bodyA.transform.position.y 
+                                    + bodyB.transform.position.z * bodyA.transform.position.z + 
+                                    (bodyB.transform.position.x + bodyB.transform.position.y + bodyB.transform.position.z)) /
+                                    Mathf.Sqrt(bodyB.transform.position.x * bodyB.transform.position.x
+                                    + bodyB.transform.position.y * bodyB.transform.position.y
+                                    + bodyB.transform.position.z * bodyB.transform.position.z);
+        return distance <= bodyA.radius;
+    }
+    bool checkSphereHalfPlaneCollision(Body bodyA, Body bodyB)
+    { 
+        Vector3 normal = transform.rotation * new Vector3(0,1,0);
+        Vector3 displacement = bodyA.transform.position - bodyB.transform.position;
+        float projection = Vector3.Dot(displacement, normal);
+        return projection < (bodyA.radius + bodyA.transform.position.y);
+    }
     private void checkCollision()
     {
         for (int i = 0; i < bodies.Count; i++)
@@ -42,19 +61,35 @@ public class ProjectileBody : MonoBehaviour
                     if (checkSphereSphereCollision(bodyA, bodyB))
                     {
                         bodyA.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                        bodyB.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                    }
+                    else
+                    {
+                        bodyA.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                        bodyB.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                    }
+                }
+                else if (bodyA.GetShape() == 0 && bodyB.GetShape() == 2) 
+                {
+                    if (checkSpherePlaneCollision(bodyA, bodyB))
+                    {
+                        bodyA.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
                     }
                     else
                     {
                         bodyA.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
                     }
                 }
-                else if (bodyA.GetShape() == 0 && bodyB.GetShape() == 2) 
-                {
-                
-                }
                 else if (bodyA.GetShape() == 0 && bodyB.GetShape() == 3)
                 {
-
+                    if (checkSphereHalfPlaneCollision(bodyA, bodyB))
+                    {
+                        bodyA.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+                    }
+                    else
+                    {
+                        bodyA.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                    }
                 }
             }
         }
@@ -65,7 +100,7 @@ public class ProjectileBody : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         checkForNewBodies();
         checkCollision();
